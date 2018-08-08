aa0694042e258f4812c1ccbd0be30f6aa418dfce
GAN JIA MIN ALVINA <17010247@rp.edu.sg>
2018-08-08 15:22:59 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/mywebbrowserenhanced/MainActivity.java
@@ -0,0 +1,40 @@
+package sg.edu.rp.c346.mywebbrowserenhanced;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLoadURL;
+    WebView wvMyPage;
+    EditText etURL;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wvMyPage = findViewById(R.id.webViewMyPage);
+        btnLoadURL = findViewById(R.id.buttonLoad);
+        etURL = findViewById(R.id.editTextURL);
+
+        wvMyPage.setWebViewClient(new WebViewClient());
+        wvMyPage.getSettings().setJavaScriptEnabled(false);
+        wvMyPage.getSettings().setAllowFileAccess(false);
+        wvMyPage.getSettings().setBuiltInZoomControls(true);
+
+        btnLoadURL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.rp.edu.sg";
+                wvMyPage.loadUrl(url);
+            }
+        });
+    }
+}
+
